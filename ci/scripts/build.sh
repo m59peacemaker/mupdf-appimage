5f98401d263e30ad00505d2cc8d125d0c71d1066
Johnny Hauser <johnnyhauser@gmail.com>
2020-06-24 20:51:51 -0500

    .

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -33,13 +33,15 @@ gcc -Wl,--no-as-needed `pkg-config --cflags --libs gtk+-2.0` platform/x11/file_c
 ( cd thirdparty/ ; git submodule init ; git submodule update )
 make prefix=/usr install DESTDIR=$(readlink -f appdir) ; find appdir/
 
-mkdir -p ./appdir/usr/share/hicolor/scalable/apps ; wget -c "https://ghostscript.com/~tor/mupdf-logo/mupdf-logo.svg" -O ./appdir/usr/share/hicolor/scalable/apps/mupdf.svg
+mkdir -p ./appdir/usr/share/hicolor/scalable/apps;
+wget -c "https://ghostscript.com/~tor/mupdf-logo/mupdf-logo.svg" -O ./appdir/usr/share/hicolor/scalable/apps/mupdf.svg
 rm -rf  ./appdir/usr/include appdir/usr/lib/*.a # Don't need to ship developer files in AppImage
 
 # MuPDF
 cp -r appdir mupdf.AppDir
 cp file_chooser mupdf.AppDir/usr/bin
-mkdir -p ./mupdf.AppDir/usr/share/applications ; cp platform/x11/mupdf.desktop ./mupdf.AppDir/usr/share/applications/
+mkdir -p ./mupdf.AppDir/usr/share/applications;
+cp platform/x11/mupdf.desktop ./mupdf.AppDir/usr/share/applications/
 ls mupdf.AppDir
 ls mupdf.AppDir/usr
 ls mupdf.AppDir/usr/bin
@@ -47,7 +49,11 @@ rm ./mupdf.AppDir/usr/bin/{mupdf-x11,muraster,mutool}
 wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt*.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
-export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
+
+# linuxdeployqt uses this for naming the AppImage file
+#export VERSION=$(git rev-parse --short HEAD)
+export VERSION="${BUILD_TAG}"
+
 ./linuxdeployqt*.AppImage ./mupdf.AppDir/usr/bin/file_chooser -bundle-non-qt-libs
 ./linuxdeployqt*.AppImage ./mupdf.AppDir/usr/share/applications/*.desktop -bundle-non-qt-libs
 ./linuxdeployqt*.AppImage ./mupdf.AppDir/usr/share/applications/*.desktop -appimage
@@ -55,9 +61,19 @@ export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for namin
 # mutool
 cp -r appdir mutool.AppDir
 rm ./mutool.AppDir/usr/bin/{muraster,mupdf*}
-mkdir -p ./mutool.AppDir/usr/share/applications ; cp platform/x11/mutool.desktop ./mutool.AppDir/usr/share/applications/
+mkdir -p ./mutool.AppDir/usr/share/applications;
+cp platform/x11/mutool.desktop ./mutool.AppDir/usr/share/applications/
 ./linuxdeployqt*.AppImage ./mutool.AppDir/usr/share/applications/*.desktop -bundle-non-qt-libs
 ./linuxdeployqt*.AppImage ./mutool.AppDir/usr/share/applications/*.desktop -appimage
 
-rm ./linuxdeployqt*.AppImage
-ls
+mupdf_AppImage_name="`echo MuPDF-*.AppImage`"
+mupdf_AppImage_path="${PWD}/${mupdf_AppImage_name}"
+
+mutool_AppImage_name="`echo mutool-*.AppImage`"
+mutool_AppImage_path="${PWD}/${mutool_AppImage_name}"
+
+echo "::set-output name=mupdf_AppImage_name::${mupdf_AppImage_name}"
+echo "::set-output name=mupdf_AppImage_path::${mupdf_AppImage_path}"
+
+echo "::set-output name=mutool_AppImage_name::${mutool_AppImage_name}"
+echo "::set-output name=mutool_AppImage_path::${mutool_AppImage_path}"
